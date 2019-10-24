409adef3f6551b41679a587d816b7dfd4f188626
Daniel <daniel@web-reality.nl>
2019-10-24 23:21:44 +0200

    fixed  no sport

--- js/setSubcribed.js
+++ js/setSubcribed.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
     sessionStorage.setItem("subscribed", true);
     console.log(sessionStorage.getItem('subscribed'));
+    setTimeout(() => {
+        document.getElementById("logNK").style.display = "none";
+        document.getElementById("bedankt").style.display = "block";
+    }, 1500);
 })
