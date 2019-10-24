fbd6b5f328b1cd52b5adc253a6b6abf31b7b8027
Daniel <daniel@web-reality.nl>
2019-10-24 16:00:51 +0200

    fucntie test

--- js/checkSubscribed.js
+++ js/checkSubscribed.js
@@ -2,12 +2,15 @@
 
 
 console.log(localStorage.getItem.subscribed)
+function check() {
+    if (localStorage.getItem('subscribed')) {
+        // $("#inschrijfform")[0].reset();
+        // document.getElementById("#inschrijfform").reset();
+        $('#inschrijfform')[0].reset();
+        $('#inschrijfform').show();
 
-if (localStorage.getItem('subscribed')) {
-    // $("#inschrijfform")[0].reset();
-    document.getElementById("#inschrijfform").reset();
-    $('#inschrijfform').show();
+    }else{
+        $('#inschrijfform').show();
+    }
 
-}else{
-    $('#inschrijfform').show();
 }
