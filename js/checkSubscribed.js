9f8aa27605cacf730695d154ac5e42b2e5610c54
Daniel <daniel@web-reality.nl>
2019-10-26 15:56:53 +0200

    d

--- js/checkSubscribed.js
+++ js/checkSubscribed.js
@@ -2,7 +2,7 @@
 
     console.log(localStorage.getItem('subscribed'));
     if (localStorage.getItem('subscribed')) {
-        $('.inschrijfformulier')[0].reset;
+        $('#Iform')[0].reset;
         localStorage.setItem("subscribed", false);
     } else {
         localStorage.setItem("subscribed", false);
