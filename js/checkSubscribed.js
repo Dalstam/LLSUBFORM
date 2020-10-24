97d486531fe6a960b238c04efe0fbd71fe53257f
Dalstam <daniel@web-reality.nl>
2020-10-24 16:58:56 +0200

    removed jquery to vanilla js

--- js/checkSubscribed.js
+++ js/checkSubscribed.js
@@ -1,10 +1,15 @@
-// var subscribed = localStorage.getItem.subscribed
-
-    console.log(sessionStorage.getItem('subscribed'));
-    if (sessionStorage.getItem('subscribed')) {
-        $('#Iform')[0].reset();
-sessionStorage.setItem("subscribed", false);
-
-    } else {
+// Check voor dubbele inschrijvingen
+console.log(sessionStorage.getItem('subscribed'));
+if (sessionStorage.getItem('subscribed') == 'true') {
+    document.querySelector('#Iform').style.display = 'none';
+    let newSub = confirm("Je hebt al een inschrijving gedaan. Wil je nog een inschrijving doen?");
+    if (newSub) {
+        document.querySelector('#Iform').reset();
         sessionStorage.setItem("subscribed", false);
+        document.querySelector('#Iform').style.display = 'block';
+    } else {
+        window.location = "https://nklittleleague.nl/";
     }
+} else {
+    sessionStorage.setItem("subscribed", false);
+}
--- js/setSubcribed.js
+++ js/setSubcribed.js
@@ -1,8 +1,7 @@
-$(document).ready(function () {
+// Nodig voor Netlify zodat de inschrijvingen afgehandeld wordt
+setTimeout(() => {
     sessionStorage.setItem("subscribed", true);
     console.log(sessionStorage.getItem('subscribed'));
-    setTimeout(() => {
-        document.getElementById("logNK").style.display = "none";
-        document.getElementById("bedankt").style.display = "block";
-    }, 1500);
-})
+    document.getElementById("logNK").style.display = "none";
+    document.getElementById("bedankt").style.display = "block";
+}, 2000);
